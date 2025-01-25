76825103fd480de065273104f320eb49a783cbd1
Talha-Siddiqui07 <156583802+Talha-Siddiqui07@users.noreply.github.com>
2025-01-25 17:15:18 +0500

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,1 +1,15 @@
+import streamlit as st
+import pandas as pd
+import joblib
 
+
+# load the pre-trained model and scaler
+model = joblib.load('svm_model.pkl')
+
+def predict_purchase(age, salary, gender):
+    # create a dataframe with the user input
+    input_data = pd.DataFrame([[age, salary, gender]], 
+                              columns=['Age','EstimatedSalary','Gender_Encoded'])
+    # make predictions
+    prediction = model.predict(input_data)
+    return prediction[0]
