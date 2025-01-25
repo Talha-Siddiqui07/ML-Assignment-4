d6394b219b521bdc299cb754dd7d283ec8d0908c
Talha-Siddiqui07 <156583802+Talha-Siddiqui07@users.noreply.github.com>
2025-01-25 17:20:40 +0500

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -13,3 +13,7 @@ def predict_purchase(age, salary, gender):
     # make predictions
     prediction = model.predict(input_data)
     return prediction[0]
+
+
+st.title("Purchase Prediction Using SVM Model")
+st.header("Want to Know if He/She will buy it for you?")
